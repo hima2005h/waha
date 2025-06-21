469cdc9b1d7701bf270b6a959098dfa9378ce868
devlikepro <waha@devlike.pro>
2025-06-21 17:33:23 +0700

    [core] no xvfb if not installed (noweb/gows image)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-# Start virtual X server in the background
-Xvfb :99 -screen 0 1280x720x24 &
-export DISPLAY=:99
-sleep 2
+# Check if Xvfb command exists
+if command -v Xvfb > /dev/null 2>&1; then
+  # Start virtual X server in the background
+  Xvfb :99 -screen 0 1280x720x24 &
+  export DISPLAY=:99
+  sleep 2
+else
+  echo "Xvfb command not found, skipping virtual X server setup"
+fi
 
 # Calculate UV_THREADPOOL_SIZE based on number of CPUs
 cpus=$(node -e "const os = require('os'); console.log(os.cpus().length);")
