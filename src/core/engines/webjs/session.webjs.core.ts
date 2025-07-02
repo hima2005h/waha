d2ab78b65c72c7603694e79cd4495e612ca9b097
devlikepro <waha@devlike.pro>
2025-07-02 11:07:41 +0700

    [core] WEBJS Up user agent

--- src/core/engines/webjs/session.webjs.core.ts
+++ src/core/engines/webjs/session.webjs.core.ts
@@ -201,6 +201,8 @@ export class WhatsappSessionWebJSCore extends WhatsappSession {
         args: this.getBrowserArgsForPuppeteer(),
         dumpio: this.isDebugEnabled(),
       },
+      userAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
       webVersion: webVersion,
       webVersionCache: {
         type: cacheType,
