35b8b7d01c230268a3149ea76d04c206bf71f064
devlikepro <waha@devlike.pro>
2025-07-22 09:58:09 +0700

    [core] 2025.7.7

--- src/version.ts
+++ src/version.ts
@@ -33,7 +33,7 @@ export function getEngineName(): string {
 }
 
 export const VERSION: WAHAEnvironment = {
-  version: '2025.7.6',
+  version: '2025.7.7',
   engine: getEngineName(),
   tier: getWAHAVersion(),
   browser:
